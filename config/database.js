396ba808c45b111a086cbb12d498aec1a43cf13f
Marcus M. Darden <mmdarden@umich.edu>
2016-01-17 00:00:28 -0500

    Add some sequelize config.

--- config/database.js
+++ config/database.js
@@ -1,26 +1,38 @@
 'use strict';
 
 var config = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "repoman_development",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  development: {
+    username: "root",
+    password: null,
+    database: "repoman_development",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    define: {
+      underscored: true,
+      freezeTableName: true
+    }
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "repoman_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  test: {
+    username: "root",
+    password: null,
+    database: "repoman_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    define: {
+      underscored: true,
+      freezeTableName: true
+    }
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "repoman_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+  production: {
+    username: "root",
+    password: null,
+    database: "repoman_production",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    define: {
+      underscored: true,
+      freezeTableName: true
+    }
   }
 };
 
