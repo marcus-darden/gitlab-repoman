ed9021a94ad9e9f9c39ac5938aaf6d599283e912
Marcus M. Darden <mmdarden@umich.edu>
2016-02-01 23:44:09 -0500

    Update table names to lower case.

--- migrations/20160130152500-create-role.js
+++ migrations/20160130152500-create-role.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Role', {
+    return queryInterface.createTable('role', {
       course_id: {
         allowNull: false,
         primaryKey: true,
@@ -26,6 +26,6 @@ module.exports = {
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Role');
+    return queryInterface.dropTable('role');
   }
 };
