a57e859be48464f7cf94853970bdd093c30b3083
Marcus M. Darden <mmdarden@umich.edu>
2016-02-03 23:21:32 -0500

    Add migration to create join tables.

---
+++ migrations/20160202004828-join-tables.js
@@ -0,0 +1,100 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.createTable('team_assignment', {
+      team_id: {
+        allowNull: false,
+        onDelete: 'restrict',
+        onUpdate: 'cascade',
+        primaryKey: true,
+        references: { model: 'team' },
+        type: Sequelize.CHAR(11)
+      },
+      assignment_id: {
+        allowNull: false,
+        onDelete: 'restrict',
+        onUpdate: 'cascade',
+        primaryKey: true,
+        references: { model: 'assignment' },
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    }).then(function() {
+      return queryInterface.createTable('user_team', {
+        user_id: {
+          allowNull: false,
+          onDelete: 'restrict',
+          onUpdate: 'cascade',
+          primaryKey: true,
+          references: { model: 'user' },
+          type: Sequelize.CHAR(8)
+        },
+        team_id: {
+          allowNull: false,
+          onDelete: 'restrict',
+          onUpdate: 'cascade',
+          primaryKey: true,
+          references: { model: 'team' },
+          type: Sequelize.CHAR(11)
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      }).then(function() {
+        return queryInterface.createTable('role', {
+          user_id: {
+            allowNull: false,
+            onDelete: 'restrict',
+            onUpdate: 'cascade',
+            primaryKey: true,
+            references: { model: 'user' },
+            type: Sequelize.CHAR(8)
+          },
+          course_id: {
+            allowNull: false,
+            onDelete: 'restrict',
+            onUpdate: 'cascade',
+            primaryKey: true,
+            references: { model: 'course' },
+            type: Sequelize.INTEGER
+          },
+          gitlab_role: {
+            allowNull: false,
+            type: Sequelize.INTEGER
+          },
+          created_at: {
+            allowNull: false,
+            type: Sequelize.DATE
+          },
+          updated_at: {
+            allowNull: false,
+            type: Sequelize.DATE
+          }
+        });
+      });
+    });
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
