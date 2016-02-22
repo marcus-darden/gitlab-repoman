4a27ea717b7c19584c4709a891d04b262da62799
Marcus M. Darden <mmdarden@umich.edu>
2016-02-21 21:46:48 -0500

    Revert ALL table names to lower case.

--- migrations/20160117145611-create-user.js
+++ migrations/20160117145611-create-user.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('User', {
+    return queryInterface.createTable('user', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -32,6 +32,6 @@ module.exports = {
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('User');
+    return queryInterface.dropTable('user');
   }
 };
--- migrations/20160117150904-create-team.js
+++ migrations/20160117150904-create-team.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('Team', {
+    return queryInterface.createTable('team', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('Team', { cascade: true });
+    return queryInterface.dropTable('team', { cascade: true });
   }
 };
